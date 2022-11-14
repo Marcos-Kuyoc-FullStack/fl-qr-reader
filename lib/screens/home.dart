389d78a1b29eed21d81a5c028284a22ac7472205
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-11-14 10:03:05 -0600

    feat: se agrega la barra de navegacion y el boton de escanear

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historial'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.delete_forever),
+          )
+        ],
+      ),
+      body: const Center(
         child: Text('Home'),
       ),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: const ScanButtom(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
