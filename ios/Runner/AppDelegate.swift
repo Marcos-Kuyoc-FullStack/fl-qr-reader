ba13dffb8f533517e6ef10df5ea93a3fe3f2084e
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-12-22 15:18:49 -0600

    chore: se agregan configuraciones de Gmaps en ios y android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCmgoIJ-Z7LekJs8zxl55UT-LJJg_m2cl0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
